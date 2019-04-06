912327ea257cd4ec45de6a0c2c0d8d829f6cdd7f
Sebastian Messmer <messmer@fb.com>
2019-04-06 01:26:23 -0700

    Add contact form

--- pages/index.js
+++ pages/index.js
@@ -68,6 +68,27 @@ const BulletsSection = () => {
         </Container>
 }
 
+const formStyle = StyleSheet.create({
+    notificationArea: {
+        marginTop: '30px',
+    },
+    notification_success: {
+        color: 'green',
+        marginTop: '10px',
+        marginBottom: '10px',
+    },
+    notification_error: {
+        color: 'red',
+        marginTop: '10px',
+        marginBottom: '10px',
+    },
+    notification_spinner: {
+        color: '#909090',
+        marginTop: '10px',
+        marginBottom: '10px',
+    },
+})
+
 const NewsletterSection = () => {
     const style = StyleSheet.create({
         registrationBox: {
@@ -76,24 +97,6 @@ const NewsletterSection = () => {
         },
         registrationButton: {
             marginLeft: '5px',
-        },
-        notificationArea: {
-            marginTop: '30px',
-        },
-        notification_success: {
-            color: 'green',
-            marginTop: '10px',
-            marginBottom: '10px',
-        },
-        notification_error: {
-            color: 'red',
-            marginTop: '10px',
-            marginBottom: '10px',
-        },
-        notification_spinner: {
-            color: '#909090',
-            marginTop: '10px',
-            marginBottom: '10px',
         },
     })
 
@@ -106,31 +109,88 @@ const NewsletterSection = () => {
                         <Label for="inputEmail" className="sr-only">Email Address:</Label>
                         <Input type="email" name="email" id="inputEmail" placeholder="Enter email" required={true} autoComplete="off" />
                     </FormGroup>
-                    <Button color="primary" className={css(style.registrationButton)}>
+                    <Button type="submit" color="primary" className={css(style.registrationButton)}>
                         Get Notified &nbsp;
                         <FontAwesomeIcon icon={faAngleDoubleRight} />
                     </Button>
                 </Form>
-                <Collapse isOpen={false} className={css(style.notificationArea)}>
+                <Collapse isOpen={false} className={css(formStyle.notificationArea)}>
                     {/*TODO Translate*/}
-                    <Collapse isOpen={false} className={`lead ${css(style.notification_success)}`}>
+                    <Collapse isOpen={false} className={`lead ${css(formStyle.notification_success)}`}>
                         Thank you. You'll get a confirmation email shortly.
                     </Collapse>
-                    <Collapse isOpen={false} className={`lead ${css(style.notification_error)}`}>
+                    <Collapse isOpen={false} className={`lead ${css(formStyle.notification_error)}`}>
                         Invalid email address.
                     </Collapse>
-                    <Collapse isOpen={false} className={`lead ${css(style.notification_error)}`}>
+                    <Collapse isOpen={false} className={`lead ${css(formStyle.notification_error)}`}>
                         You unsubscribed before and we can't resubscribe you to protect against spam. Please send an email to messmer@cryfs.org.
                     </Collapse>
-                    <Collapse isOpen={false} className={`lead ${css(style.notification_error)}`}>
+                    <Collapse isOpen={false} className={`lead ${css(formStyle.notification_error)}`}>
                         An error occurred. Please subscribe by sending an email to messmer@cryfs.org.
                     </Collapse>
                     <Collapse isOpen={false}>
-                        <FontAwesomeIcon icon={faSpinner} className={`${css(style.notification_spinner)} fa-pulse`} />
+                        <FontAwesomeIcon icon={faSpinner} className={`${css(formStyle.notification_spinner)} fa-pulse`} />
                     </Collapse>
                 </Collapse>
             </div>
         </Container>
+}
+
+const ContactSection = () => {
+    const style = StyleSheet.create({
+        content: {
+            marginTop: '20px',
+        },
+    })
+    return <Container className="text-center">
+        <h2>Contact Us</h2>
+        <div className={css(style.content)}>
+            <Col md={{size: '6', offset: 3}}>
+                <Form>
+                    {/*TODO Translate*/}
+                    <Row>
+                        <Col md="12">
+                            <FormGroup>
+                                <Label for="contact_form_message" className="sr-only">Message:</Label>
+                                <textarea id="contact_form_message" name="message" className="form-control" style={{height: '140px'}} required={true} placeholder="Your message to us. We're looking forward to your feedback, ideas and criticism. Please be blunt." />
+                            </FormGroup>
+                        </Col>
+                        <Col md="8">
+                            <FormGroup>
+                                <Label for="contact_form_email" className="sr-only">Your Email:</Label>
+                                <Input type="email" name="email" id="contact_form_email" placeholder="Your email address (optional)" autoComplete="off" />
+                            </FormGroup>
+                        </Col>
+                        <Col md={{ size: '4', offset: 0}}>
+                            <FormGroup>
+                                <Button type="submit" color="primary" block={true}>
+                                    Send &nbsp;
+                                    <FontAwesomeIcon icon={faAngleDoubleRight} />
+                                </Button>
+                            </FormGroup>
+                        </Col>
+                        <div className="clearfix" />
+                        <Col md="12">
+                            <Collapse isOpen={false} className={css(formStyle.notificationArea)}>
+                                <Collapse isOpen={false} className={`lead ${css(formStyle.notification_success)}`}>
+                                    Thank you.
+                                </Collapse>
+                                <Collapse isOpen={false} className={`lead ${css(formStyle.notification_error)}`}>
+                                    Sorry, there was an error sending your message.
+                                </Collapse>
+                                <Collapse isOpen={false} className={`lead ${css(formStyle.notification_error)}`}>
+                                    Sorry, there was an error sending your message.
+                                </Collapse>
+                                <Collapse isOpen={false}>
+                                    <FontAwesomeIcon icon={faSpinner} className={`${css(formStyle.notification_spinner)} fa-pulse`} />
+                                </Collapse>
+                            </Collapse>
+                        </Col>
+                    </Row>
+                </Form>
+            </Col>
+        </div>
+    </Container>
 }
 
 const Index = () => (
@@ -147,9 +207,7 @@ const Index = () => (
         <AlternatingSections>
             <section><BulletsSection /></section>
             <section><NewsletterSection /></section>
-            <section>third</section>
-            <section>fourth</section>
-            <section>fifth</section>
+            <section><ContactSection /></section>
         </AlternatingSections>
     </Layout>
 )
