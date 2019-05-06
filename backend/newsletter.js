5140dc6c00fe99e691cbe89a21fcc97821a3e95a
Sebastian Messmer <messmer@cryfs.org>
2019-05-05 23:49:55 -0700

    Newsletter registration returns error codes

--- backend/newsletter.js
+++ backend/newsletter.js
@@ -15,6 +15,29 @@ const mailchimp = new CachedValue(async () => {
     }
 })
 
+const response_success = {
+    statusCode: 200,
+    body: JSON.stringify({
+        'success': true,
+    }),
+}
+
+const response_error_invalid_email = {
+    statusCode: 400,
+    body: JSON.stringify({
+        'success': false,
+        'error': 'invalid-email',
+    }),
+}
+
+const response_error_unsubscribed = {
+    statusCode: 400,
+    body: JSON.stringify({
+        'success': false,
+        'error': 'unsubscribed',
+    }),
+}
+
 const do_register = async (email) => {
     const mc = await mailchimp.get()
     try {
@@ -27,26 +50,31 @@ const do_register = async (email) => {
         })
         await email_myself("CryFS Newsletter Registration", "New interested user", `Registered ${email} with newsletter`)
         console.log(`Registered ${email} with newsletter`)
+
+        return response_success
     } catch (err) {
         // ignore error due to "member already exists" but rethrow other errors
-        if (err['title'] !== 'Member Exists') {
-            console.log(JSON.stringify(err))
-            await email_myself("CryFS Newsletter Registration", "Error registering user", `Didn't register ${email} with newsletter because of an error: ${JSON.stringify(err)}`)
-            throw(err)
-        } else {
+        if (err['title'] === 'Member Exists') {
             console.log(`Didn't register ${email} because it is already registered`)
             await email_myself("CryFS Newsletter Registration", "New interested user (Already exists)", `Didn't register ${email} with newsletter because it already exists`)
+            // returning success so attackers can't see if an email address is already on the newsletter or not.
+            return response_success
+        } else if (err['title'] === 'Invalid Resource') {
+            console.log(`Didn't register ${email} because it is an invalid email address`)
+            await email_myself("CryFS Newsletter Registration", "New interested user (not adding - previously unsubscribed)", `Didn't register ${email} with newsletter because it is an invalid email address`)
+            return response_error_invalid_email
+        } else if (err['title'] === 'Forgotten Email Not Subscribed') {
+            console.log(`Didn't register ${email} because it was previously unsubscribed`)
+            await email_myself("CryFS Newsletter Registration", "New interested user (not adding - previously unsubscribed)", `Didn't register ${email} with newsletter because it was previously unsubscribed`)
+            return response_error_unsubscribed
+        } else {
+            console.log(`Didn't register ${email} with newsletter because of an error: ${JSON.stringify(err)}`)
+            await email_myself("CryFS Newsletter Registration", "Error registering user", `Didn't register ${email} with newsletter because of an error: ${JSON.stringify(err)}`)
+            throw(err)
         }
     }
 }
 
 export const register = LambdaFunction(async (body) => {
-    await do_register(body['email'])
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            'success': true,
-        }),
-    }
+    return await do_register(body['email'])
 })
